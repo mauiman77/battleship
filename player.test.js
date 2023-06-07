67e7306f34e0db6bcefa41197cee580cdfb1a2c5
mauiman77 <boerbasher@protonmail.com>
2023-06-07 22:11:33 +0100

    Create player branch and test

---
+++ player.test.js
@@ -0,0 +1,11 @@
+describe('Player creation and player gameboard', () => {
+  let player
+  let computer
+
+  let playerBoard
+  let computerBoard
+
+  xit('Create player', () => {
+    expect(player = playerTest('Human')).toMatchObject({ name: 'Human' })
+  })
+})
